499ea788f7a82fe295ae2ce9e1287e03d229fc53
kentywang <kentymail@gmail.com>
2023-07-08 16:37:40 -0400

    ch3.p6

---
+++ ch3/p6.py
@@ -0,0 +1,4 @@
+"""
+Use a hash table, implementated as an array of FIFO queues (implemented by a linked list). Each index of the array
+is a hash value based on the category of food (e.g. dairy, egg, apple) so you can get the oldest item for any food type.
+"""
