7da62b6eaef0336b7d387f528de0d0b59fc7abdf
kentywang <kentymail@gmail.com>
2023-07-22 22:09:02 -0400

    ch3.p19

--- ch3/p19.py
+++ ch3/p19.py
@@ -1,6 +1,6 @@
 """
 After each insertion or deletion, run the existing O(lg n) min and max methods and cache the results. Simply return those
-values for calls from external clients. This works because insert and delete are the old methods that can possibly alter
-the min or max, and by running a O(lg n) method within those calls which are already O(lg n), no height time complexity
+values for calls from external clients. This works because insert and delete are the only methods that can possibly alter
+the min or max, and by running a O(lg n) method within those calls which are already O(lg n), no higher  time complexity
 class is breached.
 """
