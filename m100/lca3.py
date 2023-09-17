da2c7a76f4abc8d568f96c100ad6e54a5404718a
kentywang <kentymail@gmail.com>
2023-09-17 14:35:02 -0400

    6 fb tags

---
+++ m100/lca3.py
@@ -0,0 +1,41 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+
+
+Observations:
+
+
+Cases:
+- 2 nodes
+-
+
+Option 1: [time: O(h), space: O(h)]
+- traverse to root for one node, saving each node in its path (including self) to hash set (space is O(h))
+- traverse to root on other node, checking in each node if its on the path (h checks, O(1) per check)
+- curr node is always the lowest possible ancestor, so corner case is covered. We'll never have an ancestor below curr, because we've already checked them.
+
+
+Planning    14:15-14:28 (13 min)
+Coding      14:25-14:33 (8 min)
+"""
+
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        ppath = set()
+
+        # make path to parent from one of the nodes, saving each node on the path
+        while p:
+            ppath.add(p)
+            p = p.parent
+
+        while q not in ppath:
+            q = q.parent
+
+        return q
---
+++ m100/minremovevalidparens.py
@@ -0,0 +1,95 @@
+"""
+Observations:
+- we can tolerate more ( than ), but if end up with a surplus, we need to remove from the left until we are even
+- we only consider parens within range. Reset left bound to right bound when we "zero out" our parens
+- if we have a deficit of parens (-1), we need to immediately
+- could keep two pointers [O(1)], or stack [O(n)]
+
+Option 1: Mark indices of parens that we should skip, then build "view" of string without those parens
+Time: O(n)
+Space: O(n) (s)
+- keep track of delta, ++ if "(" or -- if ")"
+- skip over alpha chars
+- when delta is 0 and we encounter a "(", we leave left index ptr and move right ptr until we can zero out the delta
+- when we zero out, we move both left and right to right + 1
+- left ptr is incremented whenever we decrement a positive delta
+- if we get get negative delta (-1), zero it, mark index as skippable, and move left and right to right + 1
+- if we end up with positive delta, mark left index as skippable, increment ptr, and decrease delta
+
+Cases:
+((((()      -> (), positive, remove left parens ()
+()(         -> (), positive but zeroed earlier. So ignore those (mark index as skippable)
+()())       -> ()() negative - remove from right immediately (mark index as skippable)
+()(()       -> ()()
+^^
+(           -> None
+)           -> None
+
+Playground:
+        abc((()
+left           ^
+right         ^
+delta   2
+skippable   XX
+----------------
+Result: abc(  )
+
+        (a(b())c
+left        ^
+right           ^
+delta   1
+skippable   X
+----------------
+Result: (a(b ))c
+
+12:58 - 13:21
+Planning: 23min (multitasking) + 10min (from earlier)
+13:21 - 13:47
+Coding: 26min
+
+Thoughts:
+Tricky in that we need to have left skip to the next parens, not just any alphas.
+"""
+
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        n = len(s)
+        left = right = 0
+        delta = 0
+        skippable = set()
+
+        def nextleftparens(i) -> int:
+            i += 1
+            while i < n and s[i] != '(':
+                i += 1
+            return i
+
+        while right < n:
+            if s[right] == '(':
+                if delta == 0:
+                    left = right  # move left up to new set of parens
+                delta += 1
+
+            elif s[right] == ')':  # so when s[right] == ')':
+                if delta == 0:  # can't have more closes than opens, so remove
+                    skippable.add(right)
+                else:  # delta is positive, so we close off leftmost parens.
+                    left = nextleftparens(left)
+                    delta -= 1
+
+            # implicit else case here is if s[right] is alpha
+            right += 1
+
+        while delta:
+            skippable.add(left)
+            left = nextleftparens(left)
+            delta -= 1
+
+        return ''.join(char for i, char in enumerate(s) if i not in skippable)
+
+
+print(Solution().minRemoveToMakeValid(")((c)d()(l"))
+print(Solution().minRemoveToMakeValid('((((()'))
+
+print(Solution().minRemoveToMakeValid('lee(t(c)o)de)'))
