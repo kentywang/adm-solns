a3fefefde87329a72da59af2baa9ef8e52e9b5a8
kentywang <kentymail@gmail.com>
2023-09-17 21:59:21 -0400

    10 fb tags

---
+++ m100/groupshiftedstrings.py
@@ -0,0 +1,53 @@
+"""
+Planning:   20:18 - 20:55   (37m)
+Coding:     - 21:03         (8m)
+One timer!
+
+Observations:
+- a group is where each member share the same letter distance between each char
+- all 1-char strings all under the 1-char group
+- if the subsequent letter's order is earlier, then keep adding 26 to it until it's later for easy dist calc
+    - would lead to more and more adding as we have a longer list, so not efficient.
+
+- regardless of if order difference is positive or negative, modding it by 26 would yield the distance.
+
+Sandbox
+------------------
+0   25  dist: (0 + 25) % 26 = 25
+a   z
+
+b   a   dist: (1 + 25) % 26 = 0
+1   0
+------------------
+a   b   d   a   z   a   dist: [1,2]
+0   1   3   26  51  52
+            ^   ^   ^
+adds of 26: 1   1   2
+------------------
+a   b   d   a   z   a   dist: [1,2]
+0   1   3   0  51  52
+            ^   ^   ^
+adds of 26: 1   1   2
+------------------
+
+Option 1:
+- for each string, find the distance in order value between each pair of adjacent elements, apply this formula:
+    [order(b) - order(a)] % 26
+- check for existence of that sequence in the table (as a hashable tuple as the key), and add the string to its value (an array) if exists, otherwise create a new one
+- Time: O(n)
+- Space: O(n) (at most a new different hash key for each string)
+"""
+from collections import defaultdict
+from itertools import pairwise
+from typing import List
+
+
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for string in strings:
+            t = tuple((ord(b) - ord(a)) % 26 for a, b in pairwise(string))
+            groups[t].append(string)
+
+        return list(groups.values())
---
+++ m100/nestedlistweightsum.py
@@ -0,0 +1,77 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+"""
+Planning:   21:11 - 21:29   (18m)
+Coding + fixing: - 21:55    (26m)
+
+Observations:
+- recursion will let us handle this generally
+- have a fixed length of 50 array, where each index corresponds to a depth. Element is the current value at that depth. We can combine mix them even if they are on different lists because of distributive property of multiplication ab + ac = a(b + c)
+- for each item we either have a num or a list, if list we recurse down and pass it the depth (O(d) space)
+- after iterating, mult the value of each element in array with its index. Sum the products.
+
+Time: O(n)
+Space: O(d)
+
+Cases:
+- [9]
+- [[[7]], 1, [4, 6]] => 1*1 + 10*2 + 7*3 = 42
+"""
+from typing import List
+
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        depthvals = [0] * 50
+
+        def recurse(nested, d):
+            for i in range(len(nested)):
+                if nested[i].isInteger():
+                    depthvals[d] += nested[i].getInteger()
+                else:
+                    recurse(nested[i].getList(), d + 1)
+
+        recurse(nestedList, 0)
+
+        return sum(v * (i + 1) for i, v in enumerate(depthvals))
+# ---------------------------------------
