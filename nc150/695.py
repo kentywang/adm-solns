586e7c8ea9df585e0b61dc814ff32e21195f0874
kentywang <kentymail@gmail.com>
2023-09-23 11:03:52 -0400

    27 fb tags

--- nc150/695.py
+++ nc150/695.py
@@ -19,11 +19,14 @@ from util import asserter
 
 class DSU:
     def __init__(self):
-        self.roots = {}
+        self.parent = {}
         self.ranks = defaultdict(lambda: 1)
 
-    def union(self, a, b):
-        r1, r2 = self.find(a), self.find(b)
+    def union_sets(self, a, b):
+        """
+        Finds roots, sets one vertex's root to the other's, and updates ranks (aka child count).
+        """
+        r1, r2 = self.find_root(a), self.find_root(b)
         # r1, r2 are now roots for a, b respectively
 
         if r1 == r2:
@@ -31,23 +34,26 @@ class DSU:
 
         # weighed union
         if self.ranks[r1] > self.ranks[r2]:
-            self.roots[r2] = r1  # smaller tree attaches to larger's root
+            self.parent[r2] = r1  # smaller tree attaches to larger's root
             self.ranks[r1] += self.ranks[r2]
         else:
-            self.roots[r1] = r2
+            self.parent[r1] = r2
             self.ranks[r2] += self.ranks[r1]
 
-    def find(self, c):
+    def find_root(self, c):
+        """
+        Return's greatest parent (aka root) of vertex, compressing the path along the way.
+        """
         # wish we could use defaultdict for this, but alas
-        if c not in self.roots:
-            self.roots[c] = c
+        if c not in self.parent:
+            self.parent[c] = c
 
-        i = self.roots[c]
+        i = self.parent[c]
 
-        while i != self.roots[i]:
+        while self.parent[i] != i:
             # path compression
-            self.roots[i] = self.roots[self.roots[i]]
-            i = self.roots[i]
+            self.parent[i] = self.parent[self.parent[i]]
+            i = self.parent[i]
 
         # found the root when root's root is root
         return i
@@ -73,10 +79,10 @@ class Solution:
 
                 if adjs:
                     for a, b in product([(i, j)], adjs):
-                        dsu.union(a, b)
+                        dsu.union_sets(a, b)
 
                 else:  # 1 cell island, manual step
-                    dsu.roots[(i, j)] = (i, j)
+                    dsu.parent[(i, j)] = (i, j)
                     _ = dsu.ranks[(i, j)]
 
         return max(list(dsu.ranks.values()) + [0])
