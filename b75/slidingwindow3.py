9dbd4a40b880e25857f67b526353ec96dd045d3a
kentywang <kentymail@gmail.com>
2023-09-25 20:14:52 -0400

    review, some blind probs

--- b75/slidingwindow3.py
+++ b75/slidingwindow3.py
@@ -1,9 +1,39 @@
+from collections import defaultdict
+
+"""
+Time: O(n)
+Space: 
+"""
+
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        return NotImplemented
+        window = defaultdict(lambda: 0)
+        getMostCommon = lambda: max(window.items(), key=lambda x: x[1])
+
+        i = 0
+        best = 0
+
+        # keep j steadily moving forward. increment i as needed to shrink
+        # window
+        for j in range(len(s)):
+            window[s[j]] += 1
+
+            most_common_letter, count = getMostCommon()
+            # 1 + j + i is the window size
+            while 1 + j - i - count > k:
+                # decr removed letter's count
+                window[s[i]] = max(0, window[s[i]] - 1)
+                most_common_letter, count = getMostCommon()
+                i += 1
+
+            # we now are within k ops, so our window is valid
+            best = max(1 + j - i, best)
+
+        return best
 
 
-print(Solution().characterReplacement("ABBB", 2))
+print(Solution().characterReplacement("ABAB", 2))
 print(Solution().characterReplacement("ABCDE", 1))
 print(Solution().characterReplacement("AABABBA", 1))
-print(Solution().characterReplacement("ABAB", 2))
+print(Solution().characterReplacement("ABBB", 2))
--- m100/trappingrain.py
+++ m100/trappingrain.py
@@ -26,3 +26,27 @@ class Solution:
                 rmax = max(rmax, height[hi])
 
         return area
+
+    def trap2(self, height: List[int]) -> int:
+        n = len(height)
+        if n <= 2:
+            return 0
+
+        lo = 0
+        hi = n - 1
+        lmax = height[lo]
+        rmax = height[hi]
+        area = 0
+
+        while lo <= hi:
+            # left side lower
+            if lmax < rmax:
+                lmax = max(lmax, height[lo])
+                area += lmax - height[lo]
+                lo += 1
+            else:  # right side lower
+                rmax = max(rmax, height[hi])
+                area += rmax - height[hi]
+                hi -= 1
+
+        return area
